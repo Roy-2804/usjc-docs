5664c9665df124e0896514e7e4747221884e7c46
Roy-2804 <royzamora2001@gmail.com>
2025-04-17 17:01:34 -0600

    Server js changes.

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
 const express = require("express");
 const path = require("path");
+
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-// Ruta a la carpeta donde está el build de React
-const buildPath = path.join(__dirname, "dist"); // o "build" si usás CRA
-
-// Servir archivos estáticos (JS, CSS, etc.)
+const buildPath = path.join(__dirname, "dist");
 app.use(express.static(buildPath));
 
-// Ruta para servir el index.html en cualquier path (React Router compatible)
 app.get("*", (req, res) => {
   res.sendFile(path.join(buildPath, "index.html"));
 });
 
-// Iniciar servidor
-app.listen(port, () => {
-  console.log(`✅ Frontend corriendo en http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
