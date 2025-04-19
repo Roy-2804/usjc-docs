0f887c59d9c66dfdc7cab52d9688cc714aaffaa2
Roy-2804 <royzamora2001@gmail.com>
2025-04-19 17:24:18 -0600

    Backend initial script .

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,8 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const auth_1 = __importDefault(require("./routes/auth"));
 const docs_1 = __importDefault(require("./routes/docs"));
 const users_1 = __importDefault(require("./routes/users"));
-dotenv_1.default.config();
+const envFile = process.env.NODE_ENV === "production" ? ".env.production" : ".env.development";
+dotenv_1.default.config({ path: envFile });
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
@@ -18,5 +19,5 @@ app.use("/api/docs", docs_1.default);
 app.use("/api/users", users_1.default);
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+    console.log(`✅ Servidor escuchando en el puerto ${PORT}`);
 });
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,19 +5,20 @@ import authRoutes from "./routes/auth";
 import docsRoutes from "./routes/docs";
 import usersRoutes from "./routes/users";
 
-dotenv.config();
+const envFile = process.env.NODE_ENV === "production" ? ".env.production" : ".env.development";
+dotenv.config({ path: envFile });
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
-
 app.use("/api/docs", docsRoutes);
-
 app.use("/api/users", usersRoutes);
 
 const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+  console.log(`✅ Servidor escuchando en el puerto ${PORT}`);
 });
